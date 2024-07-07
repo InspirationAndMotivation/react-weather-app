d98cbe49a1af1958cf1be2158d03497a3acef078
Anastasia Pranovych <pranovych.anastasiia@gmail.com>
2024-07-07 00:15:24 -0600

    Added charts

--- src/components/chartCard/ChartCard.tsx
+++ src/components/chartCard/ChartCard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ChartCard.scss';
+import { XYPlot, LineSeries } from 'react-vis';
 
 const ChartCard = () => {
   return (
     <div className="ChartCard">
       <p>Upcoming hours</p>
+      <XYPlot height={300} width={300} />
     </div>
   );
 };
--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -6,6 +6,7 @@ import WeatherInfoCard from '../weatherInfoCard/WeatherInfoCard';
 const Widget = () => {
   return (
     <div className="Widget">
+      <h3>Welcome to the Weather Service!</h3>
       <p>Check out today's weather information</p>
       <ChartCard></ChartCard>
       <p>More details of today's weather</p>
