259a4826e317a42b7dd2c5931961b0dde91948c0
Anastasia Pranovych <pranovych.anastasiia@gmail.com>
2024-10-17 15:26:06 -0600

    WA-1 Fixed secret revealing

--- src/loadOptions.ts
+++ src/loadOptions.ts
@@ -5,63 +5,6 @@ export type OptionType = {
   label: string;
 };
 
-// import axios from 'axios';
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
-//   headers: {
-//     'x-rapidapi-key': '9bad40bda9mshfdb24089665e58ep13a55djsnd07261451855',
-//     'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-// const loadOptions = (inputValue) => {
-//   return fetch(
-//     `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-//     geoApiOptions
-//   )
-//     .then((response) => response.json())
-//     .then((response) => {
-//       return {
-//         options: response.data.map((city) => {
-//           return {
-//             value: `${city.latitude} ${city.longitude}`,
-//             label: `${city.name}, ${city.countryCode}`,
-//           };
-//         }),
-//       };
-//     });
-// };
-
-// Get cities from api - https://api.api-ninjas.com/v1/city?name=San Francisco
-
-// const loadOptions = (inputValue: string) => {
-//   return dispatch(fetchTodayWeatherByCity(inputValue));
-//   // return fetch(
-//   //   `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-//   //   geoApiOptions
-//   // )
-//   //   .then((response) => response.json())
-//   //   .then((response) => {
-//   //     return {
-//   //       options: response.data.map((city) => {
-//   //         return {
-//   //           value: `${city.latitude} ${city.longitude}`,
-//   //           label: `${city.name}, ${city.countryCode}`,
-//   //         };
-//   //       }),
-//   //     };
-//   //   });
-// };
-
 const cities = [
   'Wroclaw',
   'New York',
